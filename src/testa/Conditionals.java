8d059115c7a3d118234cb4941892fd9f5c038e9d
anvita-kumar30 <kumaranvita30@gmail.com>
2022-08-12 01:41:15 +0530

    Initial commit

---
+++ src/testa/Conditionals.java
@@ -0,0 +1,13 @@
+package testa;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int age=13;
+        if(age>18){  //relational op
+            System.out.println("Yes bro you can drive!");
+        }
+        else{
+            System.out.println("No bro you cannot drive yet!");
+        }
+    }
+}
