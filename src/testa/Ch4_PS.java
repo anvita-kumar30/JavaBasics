c48b3d4831c2283ab9d4f558912f6356140fd2b3
anvita-kumar30 <kumaranvita30@gmail.com>
2022-08-16 17:45:55 +0530

    Initial commit

--- src/testa/Ch4_PS.java
+++ src/testa/Ch4_PS.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Ch4_PS {
     public static void main(String[] args) {
         //Question 1: find the value
-        int a = 10;
-        if(a=11)
-            System.out.println("I am 11");
-        else
-            System.out.println("I am not 11");
+//        int a = 10;
+//        if(a=11)
+//            System.out.println("I am 11");
+//        else
+//            System.out.println("I am not 11");
 
         //Question 2: pass or fail
         byte m1, m2, m3;  //marks is taken in 100 so byte
@@ -29,7 +29,7 @@ public class Ch4_PS {
         }
 
         //Question 3: calculate income tax
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your income in Lakhs per annum");
         float tax = 0;
         float income = sc.nextFloat();
@@ -51,46 +51,46 @@ public class Ch4_PS {
         System.out.println("The total tax paid by the employee is: " + tax);
 
         //Question 4: to find out the day of the week
-        Scanner sc = new Scanner(System.in);
-        int day = sc.nextInt();
-
-        switch (day){
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("Enter correct day number!");
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int day = sc.nextInt();
+//
+//        switch (day){
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Enter correct day number!");
+//        }
 
         //Question 5: to find whether year entered is leap year or not
-        System.out.println("Enter a year: ");
-        Scanner sc=new Scanner(System.in);
-        short year = sc.nextShort();
-        if ((year%100!=0 && year%4==0) || (year%400==0)){
-                System.out.printf("The year %d is a leap year",year);
-        }
-        else{
-                System.out.printf("The year %d is not a leap year",year);
-        }
+//        System.out.println("Enter a year: ");
+//        Scanner sc=new Scanner(System.in);
+//        short year = sc.nextShort();
+//        if ((year%100!=0 && year%4==0) || (year%400==0)){
+//                System.out.printf("The year %d is a leap year",year);
+//        }
+//        else{
+//                System.out.printf("The year %d is not a leap year",year);
+//        }
 
         //Question 6: to find out the type of website from the url
-        System.out.println("Enter the Website url: ");
-        Scanner sc = new Scanner(System.in);
-        String website = sc.next();
-        if(website.endsWith(".org")){
-            System.out.println("This is an Organizational website");
-        }
-        else if(website.endsWith(".com")){
-            System.out.println("This is a Commercial website");
-        }
-        else if(website.endsWith(".in")){
-            System.out.println("This is an Indian website");
-        }
-        else {
-            System.out.println("This is a random website");
-        }
+//        System.out.println("Enter the Website url: ");
+//        Scanner sc = new Scanner(System.in);
+//        String website = sc.next();
+//        if(website.endsWith(".org")){
+//            System.out.println("This is an Organizational website");
+//        }
+//        else if(website.endsWith(".com")){
+//            System.out.println("This is a Commercial website");
+//        }
+//        else if(website.endsWith(".in")){
+//            System.out.println("This is an Indian website");
+//        }
+//        else {
+//            System.out.println("This is a random website");
+//        }
     }
 }
