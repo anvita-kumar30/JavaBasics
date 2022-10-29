910407266f4b4b59b1ed27b08e234ec831a4c598
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-10-29 23:22:41 +0530

    Initial commit

---
+++ src/testa/Constructors.java
@@ -0,0 +1,42 @@
+package testa;
+
+class MyMainEmployee{
+    private int id;
+    private String name;
+
+    public MyMainEmployee(){
+        id = 0;
+        name = "Yoyoyo";
+    }
+    public MyMainEmployee(String myName, int myId){
+        id = myId;
+        name = myName;
+    }
+    public MyMainEmployee(String myName){
+        id = 1;
+        name = myName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int i){
+        this.id = i;
+    }
+}
+
+public class Constructors {
+    public static void main(String[] args) {
+        //MyMainEmployee anvi = new MyMainEmployee("ProgrammingWithAK", 12);
+        MyMainEmployee anvi = new MyMainEmployee();
+        //anvi.setName("AK");
+        //anvi.setId(34);
+        System.out.println(anvi.getId());
+        System.out.println(anvi.getName());
+    }
+}
