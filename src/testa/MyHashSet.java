921b70d1d6234ff13f8d3409df7334b12ec3d8ff
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-10-30 10:37:13 +0530

    Initial commit

---
+++ src/testa/MyHashSet.java
@@ -0,0 +1,15 @@
+package testa;
+
+import java.util.HashSet;
+
+public class MyHashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> myHashSet = new HashSet<>(6, 0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(3);
+        myHashSet.add(11);
+        myHashSet.add(11);
+        System.out.println(myHashSet);
+    }
+}
