61b6316789a5a12fe1e479c1885d57fe7e3645f3
Anvita <kumaranvita30@gmail.com>
2024-08-02 00:30:08 +0530

    init

---
+++ src/company/ClassesAndObjects.java
@@ -0,0 +1,11 @@
+package company;
+
+
+public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.name = "Buddy";
+        myDog.age = 3;
+        myDog.bark();
+    }
+}
---
+++ src/company/Dog.java
@@ -0,0 +1,12 @@
+package company;
+
+// Classes and Objects Example
+public class Dog {
+    String name;
+    int age;
+
+    void bark() {
+        System.out.println("Woof!");
+    }
+}
+
