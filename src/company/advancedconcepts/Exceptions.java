4109bef36918ef001850fb49e65dcd7762a51627
Anvita <kumaranvita30@gmail.com>
2024-08-02 01:00:02 +0530

    init

--- src/company/advancedconcepts/Exceptions.java
+++ src/company/advancedconcepts/Exceptions.java
@@ -1,4 +1,11 @@
 package company.advancedconcepts;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
 }
