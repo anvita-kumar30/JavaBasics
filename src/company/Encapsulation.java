5677bec9f452a03e082d8c199c5de10f05ebafca
Anvita <kumaranvita30@gmail.com>
2024-08-02 00:34:44 +0530

    init

--- src/company/Encapsulation.java
+++ src/company/Encapsulation.java
@@ -1,4 +1,13 @@
 package company;
 
+
 public class Encapsulation {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Alice");
+        person.setAge(30);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
 }
