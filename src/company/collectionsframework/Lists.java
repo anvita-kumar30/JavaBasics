4a9af89f83a5ace155bd64986a4a8bcaf89d50b7
Anvita <kumaranvita30@gmail.com>
2024-08-11 23:36:08 +0530

    init

--- src/company/collectionsframework/Lists.java
+++ src/company/collectionsframework/Lists.java
@@ -2,6 +2,7 @@ package company.collectionsframework;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 public class Lists {
     public static void main(String[] args) {
@@ -54,12 +55,59 @@ public class Lists {
         System.out.println(fruits.size()); // 0
 
 
+
         ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(10); // [10]
         numbers.add(20); // [10, 20]
         numbers.add(1, 15); // [10, 15, 20]
 
         for (Integer num : numbers) {
+            System.out.println(num);
+        }
+
+        int nums = numbers.get(1); // num = 15
+        System.out.println(nums);
+
+        numbers.set(1, 25); // [10, 25, 20]
+
+        for (Integer num : numbers) {
+            System.out.println(num);
+        }
+
+        numbers.remove(1); // [10, 20]
+
+        for (Integer num : numbers) {
+            System.out.println(num);
+        }
+
+        numbers.remove(Integer.valueOf(10)); // [20]
+
+        int size = numbers.size(); // size = 1
+        System.out.println(size);
+
+        numbers.add(25);
+        numbers.add(30);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+
+        Iterator<Integer> it = numbers.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        boolean empty = numbers.isEmpty(); // false
+        System.out.println(empty);
+
+        numbers.clear(); // []
+
+        boolean containsTen = numbers.contains(10); // false
+        System.out.println(containsTen);
+
+        numbers.add(40);
+        Object[] arr = numbers.toArray();
+        for (Object num : arr) {
             System.out.println(num);
         }
     }
